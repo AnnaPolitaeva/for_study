280568352926d0e2ad2d5cbd5c4f5c380af98a02
Anna.Politaeva <Anna.Politaeva@tatar.ru>
2026-01-29 15:27:52 +0300

    домашняя работа по ui тестам уровня junior 3

--- src/test/java/iteration2/ui/TransferMoneyByUserTest.java
+++ src/test/java/iteration2/ui/TransferMoneyByUserTest.java
@@ -267,5 +267,72 @@ public class TransferMoneyByUserTest {
         assertEquals(190.00F, accountSender.getBalance());
     }
 
-    //TODO: тест трансфера без чекбокса
+
+    @Test
+    public void userCanNotTransferMoneyByHisAccountWithoutConfirmTest(){
+        // ШАГИ ПО НАСТРОЙКЕ ОКРУЖЕНИЯ
+        // ШАГ 1: админ логинится в банке
+        // ШАГ 2: админ создает юзера
+        // ШАГ 3: юзер создает два аккаунта
+        // ШАГ 4: юзер производит пополнение аккаунта
+        // ШАГ 5: юзер логинится в банке
+        CreateUserRequest user = AdminSteps.createUser().request();
+
+        String userAuthHeader = new CrudRequester(
+                RequestSpecs.unauthSpec(),
+                Endpoint.LOGIN,
+                ResponseSpecs.requestReturnsOK())
+                .post(LoginUserRequest.builder().username(user.getUsername()).password(user.getPassword()).build())
+                .extract()
+                .header("Authorization");
+
+        CreateAccountResponse accountInfo = UserSteps.createAccount(user);
+        CreateAccountResponse secondAccountInfo = UserSteps.createAccount(user);
+        UserSteps.depositAccount(accountInfo, user, 200F);
+
+        Selenide.open("/");
+        executeJavaScript("localStorage.setItem('authToken', arguments[0]);", userAuthHeader);
+
+        Selenide.open("/dashboard");
+        $(Selectors.byClassName("welcome-text")).shouldBe(Condition.visible).shouldHave(Condition.text("Welcome, noname!"));
+
+        // ШАГИ ТЕСТА
+        // ШАГ 6: юзер переводит деньги на второй аккаунт
+        $(Selectors.byText("🔄 Make a Transfer")).click();
+        $(Selectors.byText("🆕 New Transfer")).shouldBe(Condition.visible);
+        $(".account-selector").selectOptionByValue(String.valueOf(accountInfo.getId()));
+        $(Selectors.byAttribute("placeholder", "Enter recipient account number")).sendKeys(secondAccountInfo.getAccountNumber());
+        $(Selectors.byAttribute("placeholder", "Enter amount")).sendKeys("10");
+        //кнопка транфер
+
+        // ШАГ 7: проверка, что аккаунт-получатель был пополнен, а баланс аккаунта-отправителя уменьшился в UI
+        Alert alert = switchTo().alert();
+
+        assertThat(alert.getText()).contains("✅ Successfully transferred $10 to account " + secondAccountInfo.getAccountNumber() + "!");
+
+        alert.accept();
+
+        $("option[value='" + secondAccountInfo.getId() + "']").shouldHave(text("Balance: $10.00"));
+        $("option[value='" + accountInfo.getId() + "']").shouldHave(text("Balance: $190.00"));
+
+        // ШАГ 8: проверка, что аккаунт-получатель был пополнен, а баланс аккаунта-отправителя уменьшился на API
+
+        CreateAccountResponse[] existingUserAccounts = given()
+                .spec(RequestSpecs.authAsUser(user.getUsername(), user.getPassword()))
+                .get("http://localhost:4111/api/v1/customer/accounts")
+                .then().assertThat()
+                .extract().as(CreateAccountResponse[].class);
+
+        CreateAccountResponse accountRecipient = Arrays.stream(existingUserAccounts).filter(account -> account.getAccountNumber().equals(secondAccountInfo.getAccountNumber()))
+                .findFirst().orElse(null);
+
+        CreateAccountResponse accountSender = Arrays.stream(existingUserAccounts).filter(account -> account.getAccountNumber().equals(accountInfo.getAccountNumber()))
+                .findFirst().orElse(null);
+
+        assertThat(accountRecipient).isNotNull();
+        assertEquals(10.00F, accountRecipient.getBalance());
+
+        assertThat(accountSender).isNotNull();
+        assertEquals(190.00F, accountSender.getBalance());
+    }
 }
